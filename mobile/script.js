1782292ad24277186fad3458f971b6424527e586
Zenox <giangminhtien24122009@gmail.com>
2023-09-30 20:28:26 +0700

    Add two function, updateTime and addZero. And make updateTime function work

---
+++ mobile/script.js
@@ -0,0 +1,24 @@
+function updateTime() {
+  const today = new Date();
+
+  hour = today.getHours();
+  minute = today.getMinutes();
+
+  hour = addZero(hour);
+  minute = addZero(minute);
+
+  document.getElementById("hour").innerHTML = hour;
+  document.getElementById("minute").innerHTML = minute;
+
+  setInterval(1000);
+}
+
+function addZero(number) {
+  if (number < 10) {
+    return "0" + number;
+  } else {
+    return number;
+  }
+}
+
+updateTime();
